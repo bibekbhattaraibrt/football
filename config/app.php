f3c749b52c2a46c71a6da66e925e28bba3bb8647
krossroad <krossroad07@gmail.com>
2015-09-25 17:44:24 +0545

    Added serviceprovider larvel-doctrine extension

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
          */
         LaravelDoctrine\ORM\DoctrineServiceProvider::class,
         LaravelDoctrine\Migrations\MigrationsServiceProvider::class,
+        LaravelDoctrine\Extensions\GedmoExtensionsServiceProvider::class,
     ],
 
     /*

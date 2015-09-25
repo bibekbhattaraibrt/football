5819d2d43f69b04376c23033dc22cd2c30093524
krossroad <krossroad07@gmail.com>
2015-09-25 12:54:04 +0545

    Registered service provider for Laravel-doctrine

--- config/app.php
+++ config/app.php
@@ -145,6 +145,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+            Third-party Service providers
+         */
+        LaravelDoctrine\ORM\DoctrineServiceProvider::class,
     ],
 
     /*
@@ -194,6 +198,12 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        /*
+            Third-party Alias
+         */
+        'EntityManager' => LaravelDoctrine\ORM\Facades\EntityManager::class,
+        'Registry'      => LaravelDoctrine\ORM\Facades\Registry::class,
+        'Doctrine'      => LaravelDoctrine\ORM\Facades\Doctrine::class,
     ],
 
 ];
